cb28b1854415a8cf0ae7777e68da5b71306df282
Temetski33 <teemu.laasio@gmail.com>
2025-12-13 19:22:56 +0200

    Add get function and log to console

--- ajax-assignments/t1/t1.js
+++ ajax-assignments/t1/t1.js
@@ -1,1 +1,17 @@
-console.log("hel world")
+const getUsers = async () => {
+  try {
+    const resp = await fetch(requestURL, {
+      headers: {
+        'x-api-key': 'reqres-free-v1',
+      },
+    });
+    const data = await resp.json();
+    return data;
+  } catch (err) {
+    console.error('fetch failed:', err);
+  }
+};
+
+const requestURL = 'https://reqres.in/api/users/1';
+
+getUsers(requestURL).then(user => console.log(user));
