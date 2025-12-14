00b22bd82b35192e754a58498053452f118cfb39
Temetski33 <teemu.laasio@gmail.com>
2025-12-14 13:30:12 +0200

    Add generic fetch function and tester

---
+++ ajax-assignments/t4/t4.js
@@ -0,0 +1,36 @@
+// Generic fetch function
+const fetchData = async (url, options) => {
+  try {
+    const resp = await fetch(url, options);
+
+    if (!resp.ok) {
+      throw new Error(`HTTP error! Status: ${resp.status}`);
+    }
+
+    const data = await resp.json();
+    return data;
+  } catch (err) {
+    console.error('fetch failed:', err);
+    throw err;
+  }
+};
+
+// Run test
+ try {
+     const user = {
+       name: 'John Doe',
+       job: 'Developer'
+     };
+     const url = 'https://reqres.in/api/users';
+     const options = {
+       method: 'POST',
+       headers: {
+           'Content-Type': 'application/json'
+       },
+       body: JSON.stringify(user)
+     }
+     const userData = await fetchData(url, options);
+     console.log(userData);
+   } catch (error) {
+     console.error('An error occurred:', error);
+   }
